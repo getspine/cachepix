ef0b05da010c164fe79791899c33d53ee2117cdc
Steve Salevan <steve.salevan@gmail.com>
2017-07-31 11:59:04 -0700

    Adding docker-compose improvements

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+set -ueo pipefail
+
 cd /go/src/github.com/ssalevan/photocache
+
+if [ "${PCACHE_REBUILD-false}" == true ]; then
+  glide install
+  go-wrapper install
+fi
+
 go-wrapper run "${@}"
